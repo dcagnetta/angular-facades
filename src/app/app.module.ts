6d1e6466f12b335bf93cfb114c3ae4c989023bf7
Dillan Cagnetta <dillancagnetta@yahoo.com>
2020-05-15 04:35:20 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { AppComponent } from './app.component';
+import { UserFacade } from './user.facade';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule( {
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule
+    ],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent],
+    providers: [UserFacade]
+} )
+export class AppModule {
+}
